cc267ccdb552b6aaa901661660b77e872ce38020
hughryan <hughryan@users.noreply.github.com>
2021-05-22 11:42:17 -0400

    Improve logging

--- utilities/interval.js
+++ utilities/interval.js
@@ -1,6 +1,6 @@
 const interval = (fn, timeout) => {
 	fn().then(() => {
-		console.log(`Sleeping for ${timeout}ms`);
+		console.log(`=== Sleeping for ${timeout}ms ===`);
 		setTimeout(() => interval(fn, timeout), timeout);
 	}).catch(err => {
 		console.log('Error: ', err);
--- utilities/webshot.js
+++ utilities/webshot.js
@@ -13,22 +13,22 @@ process.on('SIGTERM', onClose);
 export default async (uri) => {
 	const page = await browser.newPage();
 	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36');
-
 	await page.setViewport({
 		width: config.browserWidth,
 		height: config.browserHeight,
 	});
 
-	await page.goto(uri, {
-		timeout: config.timeoutMs,
-		waitUntil: 'networkidle0',
-	});
+	try {
+		await page.goto(uri, {
+			timeout: config.timeoutMs,
+			waitUntil: 'networkidle0',
+		});
+	} catch (err) {
+		console.log(`Timed out loading ${uri} after ${config.timeoutMs}ms`);
+	}
 
 	await page.waitForTimeout(config.waitAfterLoadMs);
-
-	console.log(`Taking snapshot of ${uri}`);
 	const shot = await page.screenshot();
-
 	await page.close();
 
 	return shot;
